094d7990aaa2ba1ced1b05f7c3cf2f95a928162d
zhongzhenwu <zhenwu369@sina.com>
2016-04-01 18:25:59 +0800

    descriptions

---
+++ weather/weather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weather
+//
+//  Created by zhenwu on 16/4/1.
+//  Copyright © 2016年 zhenwu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ weather/weather/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  weather
+//
+//  Created by zhenwu on 16/4/1.
+//  Copyright © 2016年 zhenwu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
